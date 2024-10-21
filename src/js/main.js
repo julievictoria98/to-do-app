56a8737ba9682555bf87a73d6000c364aa6dc3a2
julievictoria98 <jura0002@stud.kea.dk>
2024-10-21 11:38:54 +0200

    kan slette og tilføjet opgaver og kan rykke dem til done list

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,34 @@
 toDoArray = [
     {
         title: "Make Cake For Birthday",
+        id: crypto.randomUUID(),
         done: false
 
     },
     {
         title: "Clean Kitchen",
+        id: crypto.randomUUID(),
         done: false
     }
 ]
 
+addEventListener("DOMContentLoaded", start);
 
-addEventListener("DOMContentLoaded", showtasks);
+function start(){
+    showtasks();
+
+    
+    document.querySelector(".add_icon").addEventListener("click", () => {
+        document.querySelector("input").value = "";
+        document.querySelector(".add_task").classList.remove("hidden");  
+    
+
+});
+
+
+}
+
+
 
 const newID = self.crypto.randomUUID();
 
@@ -33,25 +50,107 @@ function addTask(value){
 
 }
 
-document.querySelector(".add_icon").addEventListener("click", () => {
-   document.querySelector(".add_task").classList.remove("hidden");  
-    
-    
-});
-function showtasks(){
-    document.querySelector("ul").innerHTML = "";
 
-    toDoArray.forEach(task =>{
+function showtasks() {
+    document.querySelector("ul").innerHTML = "";
+            document.querySelector(".done_box ul").innerHTML = "";
+
+
+
+
+    toDoArray.forEach(task => {
+
+        if(task.done === false){
+        const clone = document.querySelector("template").content.cloneNode(true);
         const list = document.querySelector("ul");
-        const li = document.createElement("li");
+        const li = clone.querySelector("li");
+        const checkMarkIcon = clone.querySelector(".checkmark_icon");
+        const deleteButton = clone.querySelector(".delete_icon");
+
         li.textContent = task.title;
-        list.appendChild(li);
+
+        list.appendChild(clone);
+
+        deleteButton.addEventListener("click", () => {
+            deleteTask(task.id);
+        });
+
+        checkMarkIcon.addEventListener("click", () => {
+            task.done = !task.done;
+            console.log(task.done);
+            // moveTodone(task.done);
+            showtasks();
+        });
+
+    }
+
+    if(task.done === true){
+        document.querySelector(".done_box ul").innerHTML = "";
+
+        const doneTasks = toDoArray.filter((task) => task.done === true)
+        doneTasks.forEach(task =>{
+        const clone = document.querySelector("template").content.cloneNode(true);
+        const doneList = document.querySelector(".done_box ul");
+        const li = clone.querySelector("li");
+        const checkMarkIcon = clone.querySelector(".checkmark_icon");
+
+        const deleteButton = clone.querySelector(".delete_icon");
+
+           deleteButton.addEventListener("click", () => {
+            deleteTask(task.id);
+        });
+
+        li.textContent = task.title;
+         checkMarkIcon.classList.add("checked");
+
+        doneList.appendChild(clone);
+        
+
+            checkMarkIcon.addEventListener("click", () => {
+            task.done = !task.done;
+            console.log(task.done);
+            // moveTodone(task.done);
+            showtasks();
+        });
+
+        // showtasks();
+
+
     })
 
-    toDoArray.forEach(element => {
-    console.log(element);
-    
-});
+    }
 
+    });
+}
+
+
+// function moveTodone(){
+//     document.querySelector(".done_box ul").innerHTML = "";
+
+//     const doneTasks = toDoArray.filter((task) => task.done === true)
+//     doneTasks.forEach(task =>{
+//         const clone = document.querySelector("template").content.cloneNode(true);
+//         const doneList = document.querySelector(".done_box ul");
+//         const li = clone.querySelector("li");
+
+//         const deleteButton = clone.querySelector(".delete_icon");
+
+//         li.textContent = task.title;
+
+//         doneList.appendChild(clone);
+
+//         showtasks();
+
+
+//     })
+
+
+// }
+
+function deleteTask(id){
+    const index = toDoArray.findIndex(task => task.id === id);
+    toDoArray.splice(index, 1);
+    showtasks();
 
 }
+
